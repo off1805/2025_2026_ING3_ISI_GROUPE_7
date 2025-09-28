78af2f889ca04d8844ffdce6d326b4024c09d191
off1805 <ouafofotsoh@gmail.com>
2025-09-28 22:10:30 +0100

    Initialisation du projet

---
+++ src/main/java/com/projetTransversalIsi/projet_transversal_isi/ProjetTransversalIsiApplication.java
@@ -0,0 +1,13 @@
+package com.projetTransversalIsi.projet_transversal_isi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetTransversalIsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetTransversalIsiApplication.class, args);
+	}
+
+}
