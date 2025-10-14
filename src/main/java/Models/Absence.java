3657058e1825b3610728fcd47e4ab024cc4476e2
InnaS._. <innasaida6@gmail.com>
2025-10-14 22:38:54 +0100

    Feature: Ajout Tables dans la base de données

--- src/main/java/Models/Absence.java
+++ src/main/java/Models/Absence.java
@@ -1,13 +1,10 @@
 package Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
+import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
+@Table(name = "abscence")
 public class Absence {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,5 +13,54 @@ public class Absence {
     private int etudiantId;
     private int professeurId;
     private int ueId;
-    private int justificatifId; //Ce champ sera null par defaut
+    private int justificatifId; // Ce champ sera null par défaut (voir note)
+
+    // Getters & Setters
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public int getEtudiantId() {
+        return etudiantId;
+    }
+
+    public void setEtudiantId(int etudiantId) {
+        this.etudiantId = etudiantId;
+    }
+
+    public int getProfesseurId() {
+        return professeurId;
+    }
+
+    public void setProfesseurId(int professeurId) {
+        this.professeurId = professeurId;
+    }
+
+    public int getUeId() {
+        return ueId;
+    }
+
+    public void setUeId(int ueId) {
+        this.ueId = ueId;
+    }
+
+    public int getJustificatifId() {
+        return justificatifId;
+    }
+
+    public void setJustificatifId(int justificatifId) {
+        this.justificatifId = justificatifId;
+    }
 }
